7032fca75e8e004a2a3bb9600aae379dd124f64a
guiandmag <guiandmag@gmail.com>
2014-02-11 18:35:41 -0200

    Commits for changing CategoryResources and ProductResources, now we can list all, by name and by id in both REST WS.

--- Delicious/src/main/java/it/delicious/view/rest/CategoryResources.java
+++ Delicious/src/main/java/it/delicious/view/rest/CategoryResources.java
@@ -3,13 +3,50 @@
  */
 package it.delicious.view.rest;
 
-import javax.ws.rs.Path;
+import java.util.List;
 
+import it.delicious.model.session.ManualCDILookup;
+import it.delicious.model.session.bean.ProductSessionBean;
+import it.delicious.persistence.entity.Category;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 /**
- * @author guilherme
+ * @author Guilherme Magalhaes - guiandmag@gmail.com
  *
  */
-@Path("/category")
-public class CategoryResources {
+@Path("category")
+public class CategoryResources extends ManualCDILookup {
+	
+	@Inject
+	private ProductSessionBean bean;
 
+	public CategoryResources(){
+		bean = getFacadeWithJNDI(ProductSessionBean.class);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path(value = "list")
+	public List<Category> getCategories(){
+		return bean.getAllCategory();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path(value = "list/{name}")
+	public List<Category> getCategoriesByName(@PathParam("name") String name){
+		return bean.getCategoriesByName(name);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path(value = "get/{id}")
+	public Category getCategoriesById(@PathParam("id") int idCategory){
+		return bean.getCategoryById(idCategory);
+	}
 }
--- Delicious/src/main/java/it/delicious/view/rest/ProductResources.java
+++ Delicious/src/main/java/it/delicious/view/rest/ProductResources.java
@@ -3,13 +3,51 @@
  */
 package it.delicious.view.rest;
 
+import java.util.List;
+
+import it.delicious.model.session.ManualCDILookup;
+import it.delicious.model.session.bean.ProductSessionBean;
+import it.delicious.persistence.entity.Product;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author Guilherme Magalhaes - guiandmag@gmail.com
  *
  */
 @Path("/product")
-public class ProductResources {
+public class ProductResources extends ManualCDILookup {
 
+	@Inject
+	private ProductSessionBean bean;
+	
+	public ProductResources(){
+		bean = getFacadeWithJNDI(ProductSessionBean.class);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path(value = "list")
+	public List<Product> getAllProducts(){
+		return bean.getAllProducts();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("list/{name}")
+	public List<Product> getProductsByName(@PathParam("name") String name){
+		return bean.getAllProductsBySpecOrName(name);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("get/{id}")
+	public Product getProductsById(@PathParam("id") int idProduct){
+		return bean.getProductById(idProduct);
+	}
 }
